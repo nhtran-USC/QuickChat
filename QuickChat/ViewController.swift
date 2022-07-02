b5eedce305a3171dcab43694e17ec64eb4c65583
Huy Tran <nhtran@usc.edu>
2022-07-01 17:02:23 -0700

    first commit

---
+++ QuickChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickChat
+//
+//  Created by Nguyen Huy Tran on 7/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
