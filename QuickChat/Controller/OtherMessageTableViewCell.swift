f5f88afc860aba29da74528eba862b45c94bfdf0
Nguyen Tran <nhtran@usc.edu>
2022-07-18 13:57:00 -0700

    bassically work no test yet

---
+++ QuickChat/Controller/OtherMessageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OtherMessageTableViewCell.swift
+//  QuickChat
+//
+//  Created by Nguyen Tran on 7/18/22.
+//
+
+import UIKit
+
+class OtherMessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
